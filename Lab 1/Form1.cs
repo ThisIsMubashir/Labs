96ed566176eeeb43a4c72b25c7415f61fd7438f7
Mubashir Hussain <m.hussain@student.unsw.edu.au>
2020-03-11 16:44:25 +1100

    Added Lab 1 supporting files

---
+++ Lab 1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ManageUser;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            User user = new User();
+            if (user.AuthenticateUser(tb_UserName.Text, tb_password.Text))
+                MessageBox.Show(user.HelloUser(tb_UserName.Text));
+            else
+                MessageBox.Show("Invalid Username / Password");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tb_UserName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Lab 1/ManageUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageUser
+{
+    public class User
+    {
+        public string HelloUser(string user) {
+            return "Hello" + user;
+        }
+
+        public bool AuthenticateUser(string user, string password) {
+            if (user == "mubashir" && password == "abc123")
+                return true;
+            else
+                return false;
+        }
+    }
+}
