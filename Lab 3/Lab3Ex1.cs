a86084a0d5b00478fb3c759141c2b070d93f5af6
Mubashir Hussain <mubashir.hussain@vit.edu.au>
2020-03-27 08:11:01 +1100

    Added Lab3 example files

---
+++ Lab 3/Lab3Ex1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        //Part b
+        //SqlCommand cmd;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Connect as windows authentication (not work for remote servers)
+            //con = new SqlConnection("server=localhost;initial catalog=NORTHWND; integrated security=SSPI");
+            //Check App.config file (Do add system.configuration as reference as well)
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["constr"].ConnectionString);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /*//Part b
+                string command = "Delete from SHIPPERS where CompanyName='5';";
+                cmd = new SqlCommand(command,con);*/
+                con.Open();
+                // Part b
+                //cmd.BeginExecuteNonQuery();
+                MessageBox.Show("Connection Open");
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ Lab 3/Lab3Ex2.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Lab3_CRUD
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        //Last section
+        SqlDataAdapter da;
+        DataSet ds;
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection("server=localhost;initial catalog=Department_Lab3;integrated security=SSPI");
+        }
+
+        void Reset()
+        {
+            tbDeptID.Text = "";
+            tbDeptName.Text = "";
+            tbDeptLocation.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string command = "select max(DeptID)+1 from Departments;";
+            cmd = new SqlCommand(command, conn);
+            conn.Open();
+            int id = (int)cmd.ExecuteScalar();
+            //string id = cmd.ExecuteScalar().ToString();
+            conn.Close();
+            tbDeptID.Text = id.ToString();
+        }
+
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string id = tbDeptID.Text;
+            string name = tbDeptName.Text;
+            string location = tbDeptLocation.Text;
+
+            string command = string.Format("Insert into departments values ({0},'{1}',{2})", id, name, location);
+            cmd = new SqlCommand(command, conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            Reset();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string id = tbDeptID.Text;
+
+            string command = "select * from departments where DeptID = " + id;
+            cmd = new SqlCommand(command, conn);
+            conn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                tbDeptName.Text = dr[1].ToString();
+                tbDeptLocation.Text = dr["Location"].ToString();
+            }
+            else {
+                Reset();
+                MessageBox.Show("Does not find the record");   
+            }
+            dr.Close();
+            conn.Close();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string id = tbDeptID.Text;
+            string name = tbDeptName.Text;
+            string location = tbDeptLocation.Text;
+
+            string command = string.Format("update departments set deptname = '{0}', location = {1} where deptid = {2};", name, location, id);
+            cmd = new SqlCommand(command, conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Record Updated");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string id = tbDeptID.Text;
+
+            string command = "delete from departments where deptid ="+id;
+            cmd = new SqlCommand(command, conn);
+            conn.Open();
+            int test = (int)cmd.ExecuteNonQuery();
+            conn.Close();
+            if (test == 0) {
+                MessageBox.Show("No department with DeptID " + id + " Retry");
+                Reset();
+            }
+            else{
+                MessageBox.Show("Record Deleted " + test.ToString());
+                Reset();
+            }
+            
+        }
+
+        private void CmbLocation_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tbDeptLocation.Text = CmbLocation.SelectedValue.ToString();
+        }
+
+        private void CmbLocation_Click(object sender, EventArgs e)
+        {
+            string command = "select * from locations_1";
+
+            da = new SqlDataAdapter(command, conn);
+            ds = new DataSet();
+            da.Fill(ds, "Locations_1");
+
+            CmbLocation.DataSource = ds.Tables["Locations_1"];
+            CmbLocation.DisplayMember = "City";
+            CmbLocation.ValueMember = "LocID";
+        }
+
+
+ 
+    }
+}
