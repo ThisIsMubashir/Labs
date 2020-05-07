91fde98b7bcf23ff91d97f7d377e3d00eb7a6c5e
Mubashir Hussain <ThisIsMubashir@users.noreply.github.com>
2020-05-08 00:57:56 +1000

    Add files via upload

---
+++ Lesson8and9/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeFirstExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SchoolContext ctx = new SchoolContext();
+            Student std = new Student();
+            std.StudentName = "Bill";
+            ctx.Students.Add(std);
+            ctx.SaveChanges();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
