d4955c2a5f1952facb6d9d78955d278b52b88155
Mubashir Hussain <mubashir.hussain@live.com>
2020-05-15 05:34:36 +1000

    Lab 9 and 10 Linq exercise

---
+++ Lab9and10/LINQ_Exercise/LINQ_Exercise/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace LINQ_Exercise
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEx1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEx2 = new System.Windows.Forms.Button();
+            this.btnEx3 = new System.Windows.Forms.Button();
+            this.btnEx4 = new System.Windows.Forms.Button();
+            this.btnEx5 = new System.Windows.Forms.Button();
+            this.btnEx6 = new System.Windows.Forms.Button();
+            this.lblEx6 = new System.Windows.Forms.Label();
+            this.lbEx6 = new System.Windows.Forms.Label();
+            this.lblEx6Ans = new System.Windows.Forms.Label();
+            this.lblEx7Ans = new System.Windows.Forms.Label();
+            this.lblEx7 = new System.Windows.Forms.Label();
+            this.btnEx7 = new System.Windows.Forms.Button();
+            this.btnEx8 = new System.Windows.Forms.Button();
+            this.lblEx8Ans = new System.Windows.Forms.Label();
+            this.lblEx8 = new System.Windows.Forms.Label();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btnEx10 = new System.Windows.Forms.Button();
+            this.btnEx11 = new System.Windows.Forms.Button();
+            this.btnEx12 = new System.Windows.Forms.Button();
+            this.btnEx13 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEx1
+            // 
+            this.btnEx1.Location = new System.Drawing.Point(37, 13);
+            this.btnEx1.Name = "btnEx1";
+            this.btnEx1.Size = new System.Drawing.Size(75, 23);
+            this.btnEx1.TabIndex = 0;
+            this.btnEx1.Text = "Ex1";
+            this.btnEx1.UseVisualStyleBackColor = true;
+            this.btnEx1.Click += new System.EventHandler(this.btnEx1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(739, 342);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnEx2
+            // 
+            this.btnEx2.Location = new System.Drawing.Point(118, 13);
+            this.btnEx2.Name = "btnEx2";
+            this.btnEx2.Size = new System.Drawing.Size(75, 23);
+            this.btnEx2.TabIndex = 2;
+            this.btnEx2.Text = "Ex2";
+            this.btnEx2.UseVisualStyleBackColor = true;
+            this.btnEx2.Click += new System.EventHandler(this.btnEx2_Click);
+            // 
+            // btnEx3
+            // 
+            this.btnEx3.Location = new System.Drawing.Point(199, 13);
+            this.btnEx3.Name = "btnEx3";
+            this.btnEx3.Size = new System.Drawing.Size(75, 23);
+            this.btnEx3.TabIndex = 3;
+            this.btnEx3.Text = "Ex3";
+            this.btnEx3.UseVisualStyleBackColor = true;
+            this.btnEx3.Click += new System.EventHandler(this.btnEx3_Click);
+            // 
+            // btnEx4
+            // 
+            this.btnEx4.Location = new System.Drawing.Point(280, 13);
+            this.btnEx4.Name = "btnEx4";
+            this.btnEx4.Size = new System.Drawing.Size(75, 23);
+            this.btnEx4.TabIndex = 4;
+            this.btnEx4.Text = "Ex4";
+            this.btnEx4.UseVisualStyleBackColor = true;
+            this.btnEx4.Click += new System.EventHandler(this.btnEx4_Click);
+            // 
+            // btnEx5
+            // 
+            this.btnEx5.Location = new System.Drawing.Point(361, 13);
+            this.btnEx5.Name = "btnEx5";
+            this.btnEx5.Size = new System.Drawing.Size(75, 23);
+            this.btnEx5.TabIndex = 5;
+            this.btnEx5.Text = "Ex5";
+            this.btnEx5.UseVisualStyleBackColor = true;
+            this.btnEx5.Click += new System.EventHandler(this.btnEx5_Click);
+            // 
+            // btnEx6
+            // 
+            this.btnEx6.Location = new System.Drawing.Point(442, 13);
+            this.btnEx6.Name = "btnEx6";
+            this.btnEx6.Size = new System.Drawing.Size(75, 23);
+            this.btnEx6.TabIndex = 6;
+            this.btnEx6.Text = "Ex6";
+            this.btnEx6.UseVisualStyleBackColor = true;
+            this.btnEx6.Click += new System.EventHandler(this.btnEx6_Click);
+            // 
+            // lblEx6
+            // 
+            this.lblEx6.AutoSize = true;
+            this.lblEx6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEx6.Location = new System.Drawing.Point(813, 113);
+            this.lblEx6.Name = "lblEx6";
+            this.lblEx6.Size = new System.Drawing.Size(0, 20);
+            this.lblEx6.TabIndex = 7;
+            // 
+            // lbEx6
+            // 
+            this.lbEx6.AutoSize = true;
+            this.lbEx6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEx6.Location = new System.Drawing.Point(781, 118);
+            this.lbEx6.Name = "lbEx6";
+            this.lbEx6.Size = new System.Drawing.Size(76, 16);
+            this.lbEx6.TabIndex = 8;
+            this.lbEx6.Text = "Exercise 6 :";
+            this.lbEx6.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblEx6Ans
+            // 
+            this.lblEx6Ans.AutoSize = true;
+            this.lblEx6Ans.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEx6Ans.Location = new System.Drawing.Point(864, 118);
+            this.lblEx6Ans.Name = "lblEx6Ans";
+            this.lblEx6Ans.Size = new System.Drawing.Size(45, 16);
+            this.lblEx6Ans.TabIndex = 9;
+            this.lblEx6Ans.Text = "label1";
+            this.lblEx6Ans.Visible = false;
+            // 
+            // lblEx7Ans
+            // 
+            this.lblEx7Ans.AutoSize = true;
+            this.lblEx7Ans.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEx7Ans.Location = new System.Drawing.Point(864, 152);
+            this.lblEx7Ans.Name = "lblEx7Ans";
+            this.lblEx7Ans.Size = new System.Drawing.Size(45, 16);
+            this.lblEx7Ans.TabIndex = 11;
+            this.lblEx7Ans.Text = "label1";
+            this.lblEx7Ans.Visible = false;
+            // 
+            // lblEx7
+            // 
+            this.lblEx7.AutoSize = true;
+            this.lblEx7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEx7.Location = new System.Drawing.Point(781, 152);
+            this.lblEx7.Name = "lblEx7";
+            this.lblEx7.Size = new System.Drawing.Size(76, 16);
+            this.lblEx7.TabIndex = 10;
+            this.lblEx7.Text = "Exercise 7 :";
+            // 
+            // btnEx7
+            // 
+            this.btnEx7.Location = new System.Drawing.Point(524, 13);
+            this.btnEx7.Name = "btnEx7";
+            this.btnEx7.Size = new System.Drawing.Size(75, 23);
+            this.btnEx7.TabIndex = 12;
+            this.btnEx7.Text = "Ex7";
+            this.btnEx7.UseVisualStyleBackColor = true;
+            this.btnEx7.Click += new System.EventHandler(this.btnEx7_Click);
+            // 
+            // btnEx8
+            // 
+            this.btnEx8.Location = new System.Drawing.Point(605, 13);
+            this.btnEx8.Name = "btnEx8";
+            this.btnEx8.Size = new System.Drawing.Size(75, 23);
+            this.btnEx8.TabIndex = 13;
+            this.btnEx8.Text = "Ex8";
+            this.btnEx8.UseVisualStyleBackColor = true;
+            this.btnEx8.Click += new System.EventHandler(this.btnEx8_Click);
+            // 
+            // lblEx8Ans
+            // 
+            this.lblEx8Ans.AutoSize = true;
+            this.lblEx8Ans.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEx8Ans.Location = new System.Drawing.Point(864, 182);
+            this.lblEx8Ans.Name = "lblEx8Ans";
+            this.lblEx8Ans.Size = new System.Drawing.Size(45, 16);
+            this.lblEx8Ans.TabIndex = 15;
+            this.lblEx8Ans.Text = "label1";
+            this.lblEx8Ans.Visible = false;
+            // 
+            // lblEx8
+            // 
+            this.lblEx8.AutoSize = true;
+            this.lblEx8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEx8.Location = new System.Drawing.Point(781, 182);
+            this.lblEx8.Name = "lblEx8";
+            this.lblEx8.Size = new System.Drawing.Size(76, 16);
+            this.lblEx8.TabIndex = 14;
+            this.lblEx8.Text = "Exercise 8 :";
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(686, 13);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(75, 23);
+            this.btn9.TabIndex = 16;
+            this.btn9.Text = "Ex9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btnEx10
+            // 
+            this.btnEx10.Location = new System.Drawing.Point(767, 13);
+            this.btnEx10.Name = "btnEx10";
+            this.btnEx10.Size = new System.Drawing.Size(75, 23);
+            this.btnEx10.TabIndex = 17;
+            this.btnEx10.Text = "Ex10";
+            this.btnEx10.UseVisualStyleBackColor = true;
+            this.btnEx10.Click += new System.EventHandler(this.btnEx10_Click);
+            // 
+            // btnEx11
+            // 
+            this.btnEx11.Location = new System.Drawing.Point(849, 13);
+            this.btnEx11.Name = "btnEx11";
+            this.btnEx11.Size = new System.Drawing.Size(75, 23);
+            this.btnEx11.TabIndex = 18;
+            this.btnEx11.Text = "Ex11";
+            this.btnEx11.UseVisualStyleBackColor = true;
+            this.btnEx11.Click += new System.EventHandler(this.btnEx11_Click);
+            // 
+            // btnEx12
+            // 
+            this.btnEx12.Location = new System.Drawing.Point(766, 52);
+            this.btnEx12.Name = "btnEx12";
+            this.btnEx12.Size = new System.Drawing.Size(75, 23);
+            this.btnEx12.TabIndex = 19;
+            this.btnEx12.Text = "Ex12";
+            this.btnEx12.UseVisualStyleBackColor = true;
+            this.btnEx12.Click += new System.EventHandler(this.btnEx12_Click);
+            // 
+            // btnEx13
+            // 
+            this.btnEx13.Location = new System.Drawing.Point(849, 52);
+            this.btnEx13.Name = "btnEx13";
+            this.btnEx13.Size = new System.Drawing.Size(75, 23);
+            this.btnEx13.TabIndex = 20;
+            this.btnEx13.Text = "Ex13";
+            this.btnEx13.UseVisualStyleBackColor = true;
+            this.btnEx13.Click += new System.EventHandler(this.btnEx13_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(935, 420);
+            this.Controls.Add(this.btnEx13);
+            this.Controls.Add(this.btnEx12);
+            this.Controls.Add(this.btnEx11);
+            this.Controls.Add(this.btnEx10);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.lblEx8Ans);
+            this.Controls.Add(this.lblEx8);
+            this.Controls.Add(this.btnEx8);
+            this.Controls.Add(this.btnEx7);
+            this.Controls.Add(this.lblEx7Ans);
+            this.Controls.Add(this.lblEx7);
+            this.Controls.Add(this.lblEx6Ans);
+            this.Controls.Add(this.lbEx6);
+            this.Controls.Add(this.lblEx6);
+            this.Controls.Add(this.btnEx6);
+            this.Controls.Add(this.btnEx5);
+            this.Controls.Add(this.btnEx4);
+            this.Controls.Add(this.btnEx3);
+            this.Controls.Add(this.btnEx2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnEx1);
+            this.Name = "Form1";
+            this.Text = "LINQ LAB Exercise";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEx1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnEx2;
+        private System.Windows.Forms.Button btnEx3;
+        private System.Windows.Forms.Button btnEx4;
+        private System.Windows.Forms.Button btnEx5;
+        private System.Windows.Forms.Button btnEx6;
+        private System.Windows.Forms.Label lblEx6;
+        private System.Windows.Forms.Label lbEx6;
+        private System.Windows.Forms.Label lblEx6Ans;
+        private System.Windows.Forms.Label lblEx7Ans;
+        private System.Windows.Forms.Label lblEx7;
+        private System.Windows.Forms.Button btnEx7;
+        private System.Windows.Forms.Button btnEx8;
+        private System.Windows.Forms.Label lblEx8Ans;
+        private System.Windows.Forms.Label lblEx8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btnEx10;
+        private System.Windows.Forms.Button btnEx11;
+        private System.Windows.Forms.Button btnEx12;
+        private System.Windows.Forms.Button btnEx13;
+    }
+}
+
---
+++ Lab9and10/LINQ_Exercise/LINQ_Exercise/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQ_Exercise
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEx1_Click(object sender, EventArgs e)
+        {
+            DafestyEntities context = new DafestyEntities();
+            //var qry = from x in context.Movies select x;
+            var qry = context.Movies.ToList();
+            //List<Movy> clst = qry.ToList<Movy>();
+            //dataGridView1.DataSource = clst;
+            dataGridView1.DataSource = qry;
+        }
+
+        private void btnEx2_Click(object sender, EventArgs e)
+        {
+            DafestyEntities context = new DafestyEntities();
+            var qry = from x in context.Movies orderby x.MovieTitle select x;
+            //var qry = context.Movies.OrderBy(x => x.MovieTitle).ToList();
+            List<Movy> clst = qry.ToList<Movy>();
+            dataGridView1.DataSource = clst;
+            //dataGridView1.DataSource = qry;
+        }
+
+        private void btnEx3_Click(object sender, EventArgs e)
+        {
+            DafestyEntities context = new DafestyEntities();
+            //var qry = from x in context.Movies orderby x.RentalPrice descending select x;
+            var qry = context.Movies.OrderByDescending(x => x.RentalPrice).ToList();
+            //List<Movy> clst = qry.ToList<Movy>();
+            //dataGridView1.DataSource = clst;
+            dataGridView1.DataSource = qry;
+        }
+
+        private void btnEx4_Click(object sender, EventArgs e)
+        {
+            DafestyEntities context = new DafestyEntities();
+            var qry = from x in context.Movies where x.Rating=="R"  select x;
+            //var qry = context.Movies.Where(x => x.Rating == "R").ToList();
+            List<Movy> clst = qry.ToList<Movy>();
+            dataGridView1.DataSource = clst;
+            //dataGridView1.DataSource = qry;
+        }
+
+        private void btnEx5_Click(object sender, EventArgs e)
+        {
+            DafestyEntities context = new DafestyEntities();
+            //var qry = from x in context.Movies where x.ProducerID == "Warner" && x.MovieType=="Sci-fi" select x;
+            var qry = context.Movies.Where(x => x.ProducerID == "Warner" && x.MovieType == "Sci-fi").ToList();
+            //List<Movy> clst = qry.ToList<Movy>();
+            //dataGridView1.DataSource = clst;
+            dataGridView1.DataSource = qry;
+        }
+
+        private void btnEx6_Click(object sender, EventArgs e)
+        {
+            DafestyEntities context = new DafestyEntities();
+            var qry = (from x in context.Movies where x.MovieType == "Action" select x).Count().ToString();
+            //var qry = context.Movies.Where(x => x.MovieType == "Action").Count().ToString();
+            lblEx6Ans.Text = qry;
+            lblEx6Ans.Visible = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEx7_Click(object sender, EventArgs e)
+        {
+            DafestyEntities context = new DafestyEntities();
+            //var qry = (from x in context.Movies where x.MovieType == "Action" select x).Sum(x=>x.TotalStock).ToString();
+            var qry = context.Movies.Where(x => x.MovieType == "Action").Sum(x=> x.TotalStock).ToString();
+            lblEx7Ans.Text = qry;
+            lblEx7Ans.Visible = true;
+            //dataGridView1.DataSource = qry.ToList();
+        }
+
+        private void btnEx8_Click(object sender, EventArgs e)
+        {
+            DafestyEntities context = new DafestyEntities();
+            var qry = (from x in context.Movies where x.MovieType == "Comedy" select x).Average(x=>x.RentalPrice).ToString();
+            //var qry = context.Movies.Where(x => x.MovieType == "Comedy").Average(y=>y.RentalPrice).ToString();
+            lblEx8Ans.Text = qry;
+            lblEx8Ans.Visible = true;
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            DafestyEntities context = new DafestyEntities();
+            //var qry = from x in context.Movies where x.Rating == "R" select new { x.VideoCode, x.MovieTitle, x.RentalPrice};
+            var qry = context.Movies.Where(x => x.Rating == "R").Select(o=>new { o.VideoCode, o.MovieTitle,o.RentalPrice});
+            //List<Movy> clst = qry.ToList<Movy>();
+            dataGridView1.DataSource = qry.ToList();
+            //dataGridView1.DataSource = qry;
+        }
+
+        private void btnEx10_Click(object sender, EventArgs e)
+        {
+            DafestyEntities context = new DafestyEntities();
+            //var qry = from x in context.Movies group x by x.MovieType into g select new { Category = g.Key, TotalStock = g.Sum(Y => Y.TotalStock) };
+            var qry = context.Movies.GroupBy(x => x.MovieType).Select(g=>new { Category = g.Key, TotalStock = g.Sum(Y => Y.TotalStock) });
+            //lblEx7Ans.Text = qry;
+            //lblEx7Ans.Visible = true;
+            dataGridView1.DataSource = qry.ToList();
+        }
+
+        private void btnEx11_Click(object sender, EventArgs e)
+        {
+            DafestyEntities context = new DafestyEntities();
+            //var qry = from x in context.Movies where x.VideoCode == 5 select new { x.MovieTitle,x.Rating,x.Producer.ProducerName};
+            var qry = context.Movies.Where(x => x.VideoCode == 5).Select(x=> new { x.MovieTitle, x.Rating,x.Producer.ProducerName });
+            //lblEx7Ans.Text = qry;
+            //lblEx7Ans.Visible = true;
+            dataGridView1.DataSource = qry.ToList();
+        }
+
+        private void btnEx12_Click(object sender, EventArgs e)
+        {
+            DafestyEntities context = new DafestyEntities();
+            //var qry = from x in context.Movies where x.ProducerID == "Walt" select new { x.MovieTitle,x.MovieType,x.Producer.ProducerName};
+            var qry = context.Movies.Where(x => x.ProducerID == "Walt").Select(x => new { x.MovieTitle,x.MovieType,x.Producer.ProducerName });
+            dataGridView1.DataSource = qry.ToList();
+        }
+
+        private void btnEx13_Click(object sender, EventArgs e)
+        {
+            DafestyEntities context = new DafestyEntities();
+            //var qry = from x in context.Movies select new { x.MovieTitle, x.MovieType, x.Producer.ProducerName };
+            var qry = context.Movies.Select(x => new { x.MovieTitle,x.MovieType,x.Producer.ProducerName });
+            dataGridView1.DataSource = qry.ToList();
+        }
+    }
+}
