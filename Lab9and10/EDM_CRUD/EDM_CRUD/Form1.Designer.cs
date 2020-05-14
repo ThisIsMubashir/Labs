16d6f0606820ab1fe503d4b07de0a3c7af69480c
Mubashir Hussain <mubashir.hussain@live.com>
2020-05-15 06:46:05 +1000

    EDM Added

---
+++ Lab9and10/EDM_CRUD/EDM_CRUD/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace EDM_CRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbVideoCode = new System.Windows.Forms.TextBox();
+            this.tbMovieTitle = new System.Windows.Forms.TextBox();
+            this.tbGenre = new System.Windows.Forms.TextBox();
+            this.tbRentalCost = new System.Windows.Forms.TextBox();
+            this.btnload = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.tbFind = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Video Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Movie Title";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(18, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Genre";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(18, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Rental Cost";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // tbVideoCode
+            // 
+            this.tbVideoCode.Location = new System.Drawing.Point(101, 23);
+            this.tbVideoCode.Name = "tbVideoCode";
+            this.tbVideoCode.Size = new System.Drawing.Size(100, 20);
+            this.tbVideoCode.TabIndex = 4;
+            // 
+            // tbMovieTitle
+            // 
+            this.tbMovieTitle.Location = new System.Drawing.Point(101, 59);
+            this.tbMovieTitle.Name = "tbMovieTitle";
+            this.tbMovieTitle.Size = new System.Drawing.Size(224, 20);
+            this.tbMovieTitle.TabIndex = 5;
+            // 
+            // tbGenre
+            // 
+            this.tbGenre.Location = new System.Drawing.Point(101, 99);
+            this.tbGenre.Name = "tbGenre";
+            this.tbGenre.Size = new System.Drawing.Size(100, 20);
+            this.tbGenre.TabIndex = 6;
+            // 
+            // tbRentalCost
+            // 
+            this.tbRentalCost.Location = new System.Drawing.Point(101, 133);
+            this.tbRentalCost.Name = "tbRentalCost";
+            this.tbRentalCost.Size = new System.Drawing.Size(100, 20);
+            this.tbRentalCost.TabIndex = 7;
+            // 
+            // btnload
+            // 
+            this.btnload.Location = new System.Drawing.Point(17, 175);
+            this.btnload.Name = "btnload";
+            this.btnload.Size = new System.Drawing.Size(75, 23);
+            this.btnload.TabIndex = 8;
+            this.btnload.Text = "Load";
+            this.btnload.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(101, 175);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(182, 175);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 10;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(263, 175);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // tbFind
+            // 
+            this.tbFind.Location = new System.Drawing.Point(101, 218);
+            this.tbFind.Name = "tbFind";
+            this.tbFind.Size = new System.Drawing.Size(100, 20);
+            this.tbFind.TabIndex = 12;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(229, 218);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 13;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 275);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.tbFind);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnload);
+            this.Controls.Add(this.tbRentalCost);
+            this.Controls.Add(this.tbGenre);
+            this.Controls.Add(this.tbMovieTitle);
+            this.Controls.Add(this.tbVideoCode);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Movie Maintenance";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbVideoCode;
+        private System.Windows.Forms.TextBox tbMovieTitle;
+        private System.Windows.Forms.TextBox tbGenre;
+        private System.Windows.Forms.TextBox tbRentalCost;
+        private System.Windows.Forms.Button btnload;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox tbFind;
+        private System.Windows.Forms.Button btnFind;
+    }
+}
+
---
+++ Lab9and10/EDM_CRUD/EDM_CRUD/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace EDM_CRUD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Reset() {
+            tbVideoCode.Text = "";
+            tbMovieTitle.Text = "";
+            tbGenre.Text = "";
+            tbRentalCost.Text = "";
+        
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            int code = Convert.ToInt32(tbFind.Text);
+            DafestyEntities ctx = new DafestyEntities();
+            Movie mv = ctx.Movies1.Where(x => x.VideoCode == code).First();
+            tbMovieTitle.Text = mv.MovieTitle;
+            tbGenre.Text = mv.Genre;
+            tbVideoCode.Text = mv.VideoCode.ToString();
+            tbRentalCost.Text = mv.RentalCost.ToString();
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int code = Convert.ToInt32(tbVideoCode.Text);
+            DafestyEntities ctx = new DafestyEntities();
+            Movie mv = ctx.Movies1.Where(x => x.VideoCode == code).First();
+            mv.MovieTitle = tbMovieTitle.Text;
+            mv.Genre = tbGenre.Text;
+            mv.RentalCost = float.Parse(tbRentalCost.Text);
+            ctx.SaveChanges();
+            MessageBox.Show("Update is successful");
+            Reset();
+
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            DafestyEntities ctx = new DafestyEntities();
+            Movie mv = new Movie();
+            Producer pd = ctx.Producers.Where(x => x.ProducerID == "Warner").First();
+            //int code = Convert.ToInt16(tbVideoCode.Text);
+            mv.VideoCode = 400;
+            mv.MovieTitle = "Sully";
+            mv.Genre = "Darama";
+            mv.ProducerID = pd.ProducerID;
+            mv.RentalCost = 2.50f;
+            mv.Rating = "U";
+            mv.TotalStock = 4;
+            ctx.Movies1.Add(mv);
+            ctx.SaveChanges();
+            MessageBox.Show("Record is successfully inserted");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DafestyEntities ctx = new DafestyEntities();
+            Movie mv = ctx.Movies1.Where(x => x.VideoCode == 400).First();
+            ctx.Movies1.Remove(mv);
+            ctx.SaveChanges();
+            MessageBox.Show("Successfully Deleted");
+        }
+    }
+}
