73086ee0c996fb21dcea134d46a50e350331243d
Mubashir Hussain <mubashir.hussain@live.com>
2020-05-29 01:20:01 +1000

    Changed the folder names

---
+++ Lab8_and_9/SchoolContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity;
+
+namespace CodeFirstExample
+{
+    class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("FormDB")
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
